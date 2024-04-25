d2061946b2b32972b94f4bb656dea8a9e000b0f2
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2024-04-25 16:59:07 -0300

    04-NestJS Routing

---
+++ Xtended/archivo.js
@@ -0,0 +1,87 @@
+[
+    {
+      "name": "Iphone 15",
+      "description": "The best smartphone in the world",
+      "price": 199.99,
+      "stock": 12,
+      "category": "smartphone"
+    },
+    {
+      "name": "Samsung Galaxy S23",
+      "description": "The best smartphone in the world",
+      "price": 150.0,
+      "stock": 12,
+      "category": "smartphone"
+    },
+    {
+      "name": "Motorola Edge 40",
+      "description": "The best smartphone in the world",
+      "price": 179.89,
+      "stock": 12,
+      "category": "smartphone"
+    },
+    {
+      "name": "Samsung Odyssey G9",
+      "description": "The best monitor in the world",
+      "price": 299.99,
+      "stock": 12,
+      "category": "monitor"
+    },
+    {
+      "name": "LG UltraGear",
+      "description": "The best monitor in the world",
+      "price": 199.99,
+      "stock": 12,
+      "category": "monitor"
+    },
+    {
+      "name": "Acer Predator",
+      "description": "The best monitor in the world",
+      "price": 150.0,
+      "stock": 12,
+      "category": "monitor"
+    },
+    {
+      "name": "Razer BlackWidow V3",
+      "description": "The best keyboard in the world",
+      "price": 99.99,
+      "stock": 12,
+      "category": "keyboard"
+    },
+    {
+      "name": "Corsair K70",
+      "description": "The best keyboard in the world",
+      "price": 79.99,
+      "stock": 12,
+      "category": "keyboard"
+    },
+    {
+      "name": "Logitech G Pro",
+      "description": "The best keyboard in the world",
+      "price": 59.99,
+      "stock": 12,
+      "category": "keyboard"
+    },
+    {
+      "name": "Razer Viper",
+      "description": "The best mouse in the world",
+      "price": 49.99,
+      "stock": 12,
+      "category": "mouse"
+    },
+    {
+      "name": "Logitech G502 Pro",
+      "description": "The best mouse in the world",
+      "price": 39.99,
+      "stock": 12,
+      "category": "mouse"
+    },
+    {
+      "name": "SteelSeries Rival 3",
+      "description": "The best mouse in the world",
+      "price": 29.99,
+      "stock": 12,
+      "category": "mouse"
+    }
+  ]
+  
